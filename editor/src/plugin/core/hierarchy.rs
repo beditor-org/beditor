d01b11862519bda117e54ef4f3ef19263c3363d9
SET001 <settydark@gmail.com>
2026-06-21 13:25:01 +0300

    refactor: cleanup

--- editor/src/plugin/core/hierarchy.rs
+++ editor/src/plugin/core/hierarchy.rs
@@ -3,16 +3,6 @@ use dioxus::prelude::*;
 
 pub fn hierarchy() -> Element {
 	let entities = use_context::<Signal<Vec<EntityInfo>>>();
-	// let protocol = use_context::<Option<Arc<Mutex<BrpProtocol<std::io::Stdout>>>>>();
-	// use_effect(move || {
-	// 	if let Some(protocol) = protocol.as_ref() {
-	// 		let protocol = protocol.clone();
-	// 		tokio::spawn(async move {
-	// 			let entities = protocol.lock().await.list_entities().await;
-	// 			info!("Received entities: {:?}", entities);
-	// 		});
-	// 	}
-	// });
 
 	rsx! {
 		h2{
