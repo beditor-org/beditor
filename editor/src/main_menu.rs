015d951d06f78a3bf2a9c7255aba0960ed458a1f
SET001 <settydark@gmail.com>
2026-07-09 00:48:34 +0300

    implement partialeq for MenuBarItemConfig to avoid having warning

--- editor/src/main_menu.rs
+++ editor/src/main_menu.rs
@@ -1,12 +1,22 @@
+use crate::event::Events;
+
 #[derive(Clone, PartialEq, Eq)]
 pub struct MenuBarGroupConfig {
 	pub label: &'static str,
 	pub items: Vec<MenuBarItemConfig>,
 }
 
-#[derive(Clone, PartialEq, Eq, Default)]
+#[derive(Clone, Default)]
 pub struct MenuBarItemConfig {
-	// pub action: Box<dyn EditorEvent>,
+	pub action: Option<fn(&Events)>,
 	pub label: &'static str,
 	pub disabled: bool,
 }
+
+impl PartialEq for MenuBarItemConfig {
+	fn eq(&self, other: &Self) -> bool {
+		self.label == other.label && self.disabled == other.disabled
+	}
+}
+
+impl Eq for MenuBarItemConfig {}
